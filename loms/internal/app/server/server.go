a8a0cd01eb9d3c535a4153a9937c1eb52cc9c465
archil.tvaladze <archt@mail.ru>
2025-09-11 21:37:25 +0300

    [hw-3] Первая попытка. Попытка угодить pipline Шаг 11

--- loms/internal/app/server/server.go
+++ loms/internal/app/server/server.go
@@ -10,7 +10,7 @@ type LomsServise interface {
 	OrderCancel(ctx context.Context, orderID int64) error
 	OrderCreate(ctx context.Context, userID int64, items []model.Item) (int64, error)
 	OrderInfo(ctx context.Context, orderID int64) (*model.Order, error)
-	OrderPay(ctx context.Context, orderId int64) error
+	OrderPay(ctx context.Context, orderID int64) error
 	StocksInfo(ctx context.Context, sku int64) (uint32, error)
 }
 
