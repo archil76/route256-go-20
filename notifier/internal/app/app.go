abd7b7b93767808791e211f91c48eecc3f1e461f
archil.tvaladze <archt@mail.ru>
2026-01-10 18:36:24 +0300

    [Hw-7] Реализован сервис notifier в сыром виде. Читает сообщения из kafka и выводит их как есть в лог. Довесок

---
+++ notifier/internal/app/app.go
@@ -0,0 +1,56 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"route256/notifier/internal/infra/config"
+	kafkaConsumer "route256/notifier/internal/infra/kafka/consumer"
+	"route256/notifier/internal/infra/logger"
+)
+
+type App struct {
+	config        *config.Config
+	consumerGroup *kafkaConsumer.KafkaConsumerGroup
+}
+
+func NewApp(configPath string) (*App, error) {
+	c, err := config.LoadConfig(configPath)
+	if err != nil {
+		return nil, fmt.Errorf("config.LoadConfig: %w", err)
+	}
+	ctx := context.Background()
+	consumerGroup, err := kafkaConsumer.NewConsumer(ctx, c.Kafka.Brokers, c.Kafka.OrderTopic, c.Kafka.ConsumerGroupID)
+	if err != nil {
+		return nil, err
+	}
+
+	app := &App{config: c, consumerGroup: &consumerGroup}
+
+	return app, nil
+}
+
+func (app *App) ListenAndServe() error {
+	defer app.consumerGroup.Close()
+
+	//go func() {
+	//	// слушаем ошибки группы в отдельной горутине
+	//	for err := range app.consumerGroup.Errors() {
+	//		logger.Errorw("ConsumerGroup error:", "err", err)
+	//	}
+	//}()
+	fmt.Printf("notifier service is ready %s:%s\n", app.config.Kafka.Host, app.config.Kafka.Port)
+	for {
+		ctx := context.Background()
+		// Метод Consume запускает обработку; после ребалансиров может возвращаться и вызываться снова
+		err := app.consumerGroup.Consume(ctx)
+		if err != nil {
+			logger.Fatalw("ConsumerGroup error:", "err", err)
+		}
+		// Если пришел сигнал прекращения, выходим из цикла
+		if ctx.Err() != nil {
+			break
+		}
+	}
+
+	return nil
+}
